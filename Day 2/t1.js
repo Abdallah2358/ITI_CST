11fe11d5a11f9d938f8c54a00e31d608e85b7b61
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-11-30 14:15:10 +0200

    day 2 done

--- Day 2/t1.js
+++ Day 2/t1.js
@@ -3,6 +3,7 @@ var letter = prompt("please enter a character");
 var isCaseSensitive = confirm("Case Sensitive ?");
 var reg = new RegExp(letter, isCaseSensitive ? '' : 'i');
 var count = 0;
+
 for (let i = 0; i < str.length; i++) {
     if (reg.test(str.charAt(i))) {
         count++;
--- Day 2/t7.js
+++ Day 2/t7.js
@@ -1,12 +1,25 @@
 
-var reg = /^[0-9]+$/;
+var reg = /^[0-9]+.?[0-9]*$/;
 do {
     var radius = prompt("please enter radius");
 } while (!reg.test(radius))
 
-alert("Area = " + (Math.PI * Math.pow(radius, 2)));
+alert("Area of circle = " + (Math.PI * Math.pow(parseFloat(radius), 2)));
 
-var reg = /^-[0-9]+$/;
+
 do {
-    var radius = prompt("please enter radius");
-} while (!reg.test(radius))
+    var num = prompt("what is the value you want to calculate its square root ?");
+} while (!reg.test(num))
+
+alert("Square root of   " + num + " is " + Math.pow(parseFloat(num), 2));
+
+
+
+do {
+    var rad = prompt("what is the angle you want to calculate cosine to ?");
+} while (!reg.test(rad));
+
+document.write("<h1>Cosine " + rad + "° = " + Math.cos(parseInt(rad) * Math.PI / 180).toFixed(2) + "</h1>");
+
+
+

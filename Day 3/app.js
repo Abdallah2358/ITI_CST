93fd09174f472e04e3a35bf1f14f92be97ac3833
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-11-30 14:15:21 +0200

    day 3 started

---
+++ Day 3/app.js
@@ -0,0 +1,20 @@
+var win;
+function fly(params) {
+    win = open("flying.html", "", "width=200,height=150")
+    var x = 1, y = 1;
+    setInterval(function () {
+
+        win.moveBy(x, y)
+        clg
+    }, 20)
+
+
+}
+
+function type(params) {
+
+}
+
+function scroll(params) {
+
+}

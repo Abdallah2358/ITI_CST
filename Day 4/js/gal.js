2e7a6208988b6cddd54832dac77d5c999290eed3
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-12-06 13:53:00 +0200

    task 2

---
+++ Day 4/js/gal.js
@@ -0,0 +1,32 @@
+
+var img = document.getElementById('img')
+var start = 2;
+var timerId;
+function newSource(n) {
+    img.src = "images/" + n + ".jpg"
+}
+
+function next() {
+    if (start < 7) {
+        newSource(start++)
+    }
+}
+function prev() {
+    if (start > 0) {
+        newSource(start--)
+    }
+}
+function ss() {
+    timerId = setInterval(function () {
+        if (start == 7) {
+            start = 1;
+            next()
+        } else {
+            next()
+        }
+    }, 1000)
+
+}
+function stop() {
+    clearInterval(timerId);
+}
